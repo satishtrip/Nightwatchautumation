d870cfa636568a0598bf5adeb7f3789f89aa561f
satishtrip <satish.tripathi1297@gmail.com>
2021-01-04 22:26:54 +0530

    submit search

--- tests/google.js
+++ tests/google.js
@@ -1,11 +1,23 @@
 module.exports ={
     '@tags': ['google'],
-    'Google search for satish'(browser){
+    'Google search  for : satish'(browser){
         const mainQuery = "satish tripathi";
         const mainQueryInputSelector = 'input[name="as_q"]'
+        const languageDropDownOptions = '#lr_button';
+        const languageDropDownOptionsSelectors = '.goog-menuitem[value = "lang_hi"]';
+        const lastupdatedropdownOpenerSelector = '#as_qdr_button'
+        const lastupdatedropdownValueSelector = '.goog-menuitem[value = "w"]'
+        const submitButtonSelector = '.jfk-button[type = "submit"]'
         browser
         .url('https://www.google.co.in/advanced_search')
         .setValue(mainQueryInputSelector, mainQuery)
+        .click(languageDropDownOptions)
+        .click(languageDropDownOptionsSelectors)
+        .click(lastupdatedropdownOpenerSelector)
+        .click(lastupdatedropdownValueSelector)
+        .click(submitButtonSelector)
         .saveScreenshot('tests_output/google.png')
     }
 }
+
+ 
